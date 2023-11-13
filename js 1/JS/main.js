9d394163dbaa8b70245ddce18363fa65c3a96d3f
sana0000 <72174048+sana0000@users.noreply.github.com>
2023-11-14 00:14:57 +0200

    Add files via upload

---
+++ js 1/JS/main.js
@@ -0,0 +1,42 @@
+
+var x=document.getElementById('example');
+
+x.innerHTML="hello from java file";
+x.style.color= "rgb(255,0,0)";
+x.style.textAlign= "center";
+
+
+// document.getElementById("example") .innerHTML = "hello from java file";
+
+// document.getElementById("example") .style.color="red";
+
+// document.getElementById("example") .style.textAlign="center";
+
+// alert("sanaa essam");
+
+// console.log ("ahmed ali");
+
+// document.write("aaaaa");
+
+
+var x=5;
+var y=6;
+var z= x+y;
+
+console.log(z);
+
+
+var counter=1;
+
+function increment(){
+    document.getElementById("sanaa").innerHTML=counter;
+     counter++
+
+}
+
+function decrement(){
+    document.getElementById("sanaa").innerHTML=counter;
+    counter--;
+
+
+}
